3bea6b6290f2671f3a5ea86d89ed4d0797159520
Ryan <griffinryan77@gmail.com>
2023-12-15 19:30:54 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(Ralph){
+    cats.push("Ralph");
+    return Ralph
+}
+
+function destructivelyPrependCat(Bob){
+    cats.unshift("Bob");
+    return Bob
+}
+
+function destructivelyRemoveLastCat(Garfield){
+    cats.pop(Garfield);
+    return Garfield
+}
+
+function destructivelyRemoveFirstCat(Milo){
+    cats.shift(Milo);
+    return Milo
+}
+
+function appendCat(Broom){
+    const kitty = [...cats, "Broom"];
+    return kitty
+}
+
+function prependCat(Arnold){
+    const allCities = ["Arnold", ...cats];
+    return allCities
+    
+}
+
+function removeLastCat(Garfield){
+    const Cake = cats.slice();
+    Cake.pop();
+    return Cake
+}
+
+function removeFirstCat(Milo){
+    const Bake = cats.slice();
+    Bake.shift();
+    return Bake
+}
+
+console.log(cats)
+console.log(destructivelyAppendCat(Ralph))
+console.log(destructivelyPrependCat(Bob))
+console.log(destructivelyRemoveLastCat(Garfield))
+console.log(destructivelyRemoveFirstCat(Milo))
+console.log(appendCat(Broom))
+console.log(prependCat(Arnold))
+console.log(removeLastCat(Garfield))
+console.log(removeFirstCat(Milo))
